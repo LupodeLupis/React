c2fe0193ddc806bd7502b9babf2015edac0bd307
LupodeLupis <alaurenzi06@gmail.com>
2019-10-05 21:24:22 -0400

    working on redux

--- Redux/src/index.js
+++ Redux/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import reducer from './store/reducer'
 import { createStore } from "redux";
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux'
 
-const store = createStore()
+const store = createStore(reducer)
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
